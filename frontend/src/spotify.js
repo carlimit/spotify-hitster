744c58544dee1edcadc2ef1554ca1eed78acc138
carlimit <carlnasse60@gmail.com>
2026-02-11 21:01:21 +0100

    update redirect uri

--- frontend/src/spotify.js
+++ frontend/src/spotify.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = "c6cc6b6bb4a5414781ab9d9f3baf416e";
-const REDIRECT_URI = "https://localhost:5173";
+const REDIRECT_URI = "https://spotify-hitster.vercel.app/";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
 
